c7de0a19580f7d767d1bf0977ef6a98e90b8d1f2
Brayan D Rodriguez P <bdrp0528@gmail.com>
2025-11-09 22:28:22 -0500

    segundo-commit

---
+++ Punto-11.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+
+const nums = [1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1];
+let contador = 0;
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === 1) {
+    contador++;
+  }
+}
+
+console.log(contador);
--- Punto-9.js
+++ Punto-9.js
@@ -1,3 +1,13 @@
 const prompt = require('prompt-sync')();
 
-let 
+let numero = Number(prompt("Ingresa un número:"));
+let numeros = [];
+
+for (let i = 1; i <= numero; i++) {
+  numeros.push(i);
+}
+
+numeros.splice(1, 1); 
+for (let i = 0; i < numeros.length; i++) {
+  console.log(numeros[i]);
+}
